eee49ae9b532d1fdba30374352907d4d6c3ca20f
David Ezekiel <mrdavezy@gmail.com>
2020-10-22 21:20:27 +0100

    This is my Data_Science_Capstone_Assignment

---
+++ Assignment/Coursera_Capstone.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Coursera_Capstone
+# 
+# This notebook will be mainly used for the capstone project.
+
+# In[1]:
+
+
+import numpy as np  # useful for many scientific computing in Python
+import pandas as pd # primary data structure library
+
+
+# In[2]:
+
+
+print ("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
